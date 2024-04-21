390d51ee2d50a5d805c9230a47779e19c9598772
tholliver <michaelmmv@outlook.com>
2024-04-21 09:39:20 -0400

    ... v4

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-(0, server_1.StartServer)().then(() => {
-    console.log("Server is up");
-});
--- dist/typos/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eGroupBy = void 0;
-var eGroupBy;
-(function (eGroupBy) {
-    eGroupBy["Day"] = "day";
-    eGroupBy["Month"] = "month";
-    eGroupBy["Year"] = "year";
-})(eGroupBy || (exports.eGroupBy = eGroupBy = {}));
